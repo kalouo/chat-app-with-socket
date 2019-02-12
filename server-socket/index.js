9cc076d6e88df20d3ad689f64dd14937c77bb935
okalouti <kalouo@pm.me>
2019-02-12 10:07:14 +0900

    configures basic chat-app with socket

---
+++ server-socket/index.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-console */
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const server = app.listen(PORT, function() {
+  console.log("Server running on port ", PORT);
+});
+
+const io = require("socket.io")(server);
+io.on("connection", function(socket) {
+  console.log(socket.id);
+  socket.on("SEND_MESSAGE", function(data) {
+    io.emit("MESSAGE", data);
+  });
+});
